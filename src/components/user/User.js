8eb8e7114f80fb1568c5780c723b6db5dc4376a9
IsmaelEzequiel <kell.trollhammaren@hotmail.com>
2017-11-01 09:05:56 -0300

    Changed class name and removed link undercore

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -6,16 +6,18 @@ import './User.css';
 const UserAvatar = ({ avatarUrl, avatarImage, name }) => {
   return (
     <div className="avatar">
-      <a className="avatar_link" href={avatarUrl}>
-        <div className="avatar_thumb" style={{ backgroundImage: `url(${avatarImage})`  }}></div>
-        <span className="avatar_name">{ name }</span>
+      <a className="avatar-link" href={avatarUrl}>
+        <div className="avatar-thumb" style={{ backgroundImage: `url(${avatarImage})`  }}></div>
+        <span className="avatar-name">{ name }</span>
       </a>
     </div>
   );
 };
 
 UserAvatar.propTypes = {
-  name: PropTypes.string
+  name: PropTypes.string,
+  avatarUrl: PropTypes.string,
+  avatarImage: PropTypes.string
 }
 
 export default UserAvatar;
